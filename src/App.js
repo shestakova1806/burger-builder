eed3b72dee98c0c23655c6280b1791aba68a4afb
Alexandra Shestakova <shestakova18@bk.ru>
2020-12-20 15:30:55 +0300

    feat: created the structure of the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div></div>;
-}
+import React, { Component } from "react";
 
-export default App;
+import { Layout } from "./components/Layout/Layout";
+import { BurgerBuilder } from "./containers/BurgerBuilder/BurgerBuilder";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
+}
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Aux } from "../../hoc/Aux";
+import classes from "./Layout.module.css";
+
+export const Layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
