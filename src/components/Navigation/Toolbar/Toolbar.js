3d99aa6f7ef436b51433514f489169c4827676fd
Alexandra Shestakova <shestakova18@bk.ru>
2021-01-07 23:12:55 +0500

    feat: added toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from "./Toolbar.module.css";
+
+export const Toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>...</nav>
+  </header>
+);
