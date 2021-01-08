2b091366f160510620089cf4ac2ee2344bfd82b0
Alexandra Shestakova <shestakova18@bk.ru>
2021-01-08 18:49:59 +0500

    feat: added reusable navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import classes from "./Toolbar.module.css";
 import { Logo } from "../../Logo/Logo";
+import { NavigationItems } from "../NavigationItems/NavigationItems";
+import { NavigationItem } from "../NavigationItems/NavigationItem/NavigationItem";
 
 export const Toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
     <Logo />
-    <nav>...</nav>
+    <NavigationItems />
   </header>
 );
