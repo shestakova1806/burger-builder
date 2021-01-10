dcd5769214b6d3deecf87ad06d76d66130ad6b24
Alexandra Shestakova <shestakova18@bk.ru>
2021-01-10 17:45:49 +0500

    refactor: changed folders structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import { Logo } from "../../Logo/Logo";
 import { NavigationItems } from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.css";
 import { Backdrop } from "../../UI/Backdrop/Backdrop";
-import { Aux } from "../../../hoc/Aux";
+import { Aux } from "../../../hoc/Aux/Aux";
 
 export const SideDrawer = (props) => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 
 import classes from "./Modal.module.css";
-import { Aux } from "../../../hoc/Aux";
+import { Aux } from "../../../hoc/Aux/Aux";
 import { Backdrop } from "../Backdrop/Backdrop";
 
 export class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return nextProps.show !== this.props.show;
-  }
-
-  componentDidUpdate() {
-    console.log("[Modal] DidUpdate");
   }
 
   render() {
