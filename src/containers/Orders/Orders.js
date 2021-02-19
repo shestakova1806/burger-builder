29cc481d371a85d33cc2122f28c09565b89e1d4a
Alexandra Shestakova <shestakova18@bk.ru>
2021-02-20 01:47:58 +0500

    feat: added orders page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import { Order } from "../../components/Order/Order";
+
+export class Orders extends Component {
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
+  }
+}
